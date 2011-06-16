3447dcc26e5ef10244bda23dcf91c8c72eb58955
Stephan Schubert <stephan@frozencherry.de>
2011-06-16 20:01:55 +0200

    Grant access to the engine's helpers to the main app.

--- lib/blog/engine.rb
+++ lib/blog/engine.rb
@@ -11,6 +11,18 @@ module Blog
       g.test_framework  :rspec
     end
 
+    # The main app should be able to use the engine's helpers.
+
+    initializer "blog.grant_helper_access" do |app|
+      ActiveSupport.on_load(:action_controller) do
+        helper Blog::Engine.helpers
+      end
+    end
+
+    # On the other hand, the engine should be able to use at least
+    # some of the main app's helpers to ensure basic support for
+    # rendering a single layout for both.
+
     initializer "blog.include_main_app_helpers" do |app|
       ActiveSupport.on_load(:action_view) do
         require app.root + 'app/helpers/application_helper'
