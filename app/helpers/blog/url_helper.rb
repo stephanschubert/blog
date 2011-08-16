acf6b5775e38c4beb1952c85eb878cf844f59cd4
Stephan Schubert <stephan@frozencherry.de>
2011-08-16 21:51:19 +0200

    [BUGFIX] Missing options

--- app/helpers/blog/url_helper.rb
+++ app/helpers/blog/url_helper.rb
@@ -9,25 +9,25 @@ module Blog
     end
 
     def public_post_path(post, options = {})
-      main_app.slug_path(post)
+      main_app.slug_path(post, options)
     rescue
       blog.slug_path parameters_for_post_path(post, options)
     end
 
     def public_post_url(post, options = {})
-      main_app.slug_url(post)
+      main_app.slug_url(post, options)
     rescue
       blog.slug_url parameters_for_post_path(post, options)
     end
 
     def public_tag_path(tag, options = {})
-      main_app.slug_path(tag)
+      main_app.slug_path(tag, options)
     rescue
       blog.posts_by_tag_path(tag, options)
     end
 
     def public_tag_url(tag, options = {})
-      main_app.slug_url(tag)
+      main_app.slug_url(tag, options)
     rescue
       blog.posts_by_tag_url(tag, options)
     end
